3ee79e130e4e9c702146a7522608662bbb29482b
Haywhiz <babarindeademola@gmail.com>
2023-01-25 19:27:19 +0100

    blocked-scope

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,10 @@
+export default function taskBlock(trueOrFalse) {
+  if (trueOrFalse) {
+    const task = true;
+    const task2 = false;
+  }
+  const task = false;
+  const task2 = true;
+
+  return [task, task2];
+}
